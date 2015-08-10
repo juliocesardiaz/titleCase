2fdabe4f6f726b84b07c1d4d88b42da42d8fdc3a
Julio Diaz & Adam Won <student@epicodus.com>
2015-08-10 12:18:21 -0700

    finished app

--- app/app.php
+++ app/app.php
@@ -14,6 +14,9 @@
 
     $app->get("/view_title_case", function() use ($app) {
         $my_TitleCaseGenerator = new TitleCaseGenerator;
-        $title_cased_phrase = $my_TitleCase
-    })
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
+    });
+    
+    return $app;
 ?>
